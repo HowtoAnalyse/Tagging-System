aa2728afa1bfc85e9adbeb1ae22071fdd886df15
Joey14123142 <14123142d@connect.polyu.hk>
2017-11-11 09:52:34 +0800

    Added user profile

--- tagging/urls.py
+++ tagging/urls.py
@@ -1,4 +1,4 @@
-"""test URL Configuration
+"""tagging URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/qa'
 
 urlpatterns = [
-	url(r'^labeler/', include('labeler.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^qa/', include('qa.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+	url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
